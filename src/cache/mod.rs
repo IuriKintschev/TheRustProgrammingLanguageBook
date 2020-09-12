10966ac6638d9ed20cc6dfb4cf39d0bdd61e82c7
Iuri_Kintschev <jtc.iuri07@gmail.com>
2020-09-12 14:29:22 -0700

    Testando smart pointers

---
+++ src/cache/mod.rs
@@ -0,0 +1,64 @@
+use std::{collections::HashMap, thread, time};
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: HashMap<u32, u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: HashMap::new(),
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        if self.value.contains_key(&arg) {
+            let res = self.value.get(&arg).expect("Nenhum tratamento");
+
+            return *res;
+        }
+        let v = (self.calculation)(arg);
+        let res = self.value.entry(v).or_insert(v);
+        return *res;
+    }
+}
+
+pub fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expressive_closure = Cacher::new(|num| {
+        println!("Calculation Slowy...");
+        thread::sleep(time::Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expressive_closure.value(intensity));
+        println!("Next, do {} situps!", expressive_closure.value(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expressive_closure.value(intensity)
+            );
+        }
+    }
+}
+
+#[test]
+fn call_with_different_values() {
+    let mut c = Cacher::new(|a| a);
+
+    let v1 = c.value(1);
+    let v2 = c.value(2);
+    println!("{}", v1);
+
+    assert_eq!(v2, 2);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,75 @@
-mod search;
-
-use search::init;
-
-fn main() {
-    init();
+pub trait Messenger {
+    fn send(&self, msg: &str);
 }
+
+pub struct LimitTracker<'a, T: Messenger> {
+    messeger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+
+impl<'a, T> LimitTracker<'a, T>
+where
+    T: Messenger,
+{
+    fn new(messeger: &T, max: usize) -> LimitTracker<T> {
+        LimitTracker {
+            messeger,
+            value: 0,
+            max,
+        }
+    }
+
+    fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let percent_of_max = self.value as f64 / self.max as f64;
+
+        if percent_of_max >= 1.0 {
+            self.messeger.send("Error, You are over your quota!");
+        } else if percent_of_max >= 0.9 {
+            self.messeger
+                .send("Urgent warning: You've used up over 90% of your quota!");
+        } else if percent_of_max >= 0.75 {
+            self.messeger
+                .send("Warning: You've used up over 75% of your quota!");
+        }
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::{LimitTracker, Messenger};
+    use std::cell::RefCell;
+
+    struct MockMessegr {
+        sent_messeger: RefCell<Vec<String>>,
+    }
+
+    impl MockMessegr {
+        fn new() -> MockMessegr {
+            MockMessegr {
+                sent_messeger: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessegr {
+        fn send(&self, msg: &str) {
+            self.sent_messeger.borrow_mut().push(String::from(msg));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_message = MockMessegr::new();
+        let mut limit_tracker = LimitTracker::new(&mock_message, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_message.sent_messeger.borrow().len(), 1);
+        assert_eq!("Warning: You've used up over 75% of your quota!", mock_message.sent_messeger.borrow()[0]);
+    }
+}
+
+fn main() {}
