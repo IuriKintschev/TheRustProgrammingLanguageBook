722c8628890a3ae9dd16014cf7172c1832483824
Iuri_Kintschev <jtc.iuri07@gmail.com>
2020-09-14 13:26:06 -0700

    Rust patern orientado a objetos

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-mod gui;
-use gui::example_screen;
+mod post;
+
+use post::Post;
 
 fn main() {
-    example_screen();
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
