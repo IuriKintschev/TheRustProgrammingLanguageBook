0e2c3392d97cba42cce2fa8b5da83daeca901a75
Iuri_Kintschev <jtc.iuri07@gmail.com>
2020-09-15 15:42:33 -0700

    implementando pool de threads

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,4 @@
-mod post;
-
-use post::Post;
-
+mod server;
 fn main() {
-    let mut post = Post::new();
-
-    post.add_text("I ate a salad for lunch today");
-
-    let post = post.request_review();
-
-    let post = post.approve();
-
-    assert_eq!("I ate a salad for lunch today", post.content());
+    server::init();
 }
---
+++ src/server/mod.rs
@@ -0,0 +1,49 @@
+mod pool_thread;
+
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+use std::{fs, thread, time};
+use pool_thread::ThreadPool;
+
+pub fn init() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    // loop from conections
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    // routes
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    // reponses
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        // slow request simulation
+        thread::sleep(time::Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    // read file body
+    let contents = fs::read_to_string(filename).unwrap();
+
+    // reeponse concat with status request
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
